408e18a54924e040fb0ae34d74e9790f82b6fdcd
Kirthi Pasala <kirthipasala@Kirthis-MacBook-Air.local>
2026-04-25 09:57:37 +0530

    string character index

--- pic.js
+++ pic.js
@@ -1,2 +1,5 @@
-# practice-character-checker
-character checker
+const readlinesync = require("readline-sync");
+let user = readlinesync.question("enter a string")
+let index = readlinesync.question("enter an index number: ");
+let character = user[index];
+console.log("character index is" + index +  ' '  + character);
